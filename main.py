41f63da4cbf7d7d9d95c4af549deeed719c8f522
Keith <germanbohler@gmail.com>
2023-01-23 20:39:42 -0600

    moved to main

--- main.py
+++ main.py
@@ -12,13 +12,26 @@ def clean_barcode(ScannedBarcode: str) -> int:
     CleanedBarcode = ScannedBarcode.strip("\t")
     return int(CleanedBarcode)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print("Starting... ")
+'''
+Purpose: Check if a scanned barcode is unique or increment counter.
+Precondition: Decalred empty or loaded dictionary.
+Postcondition: Dictionary has new entry or existing entry in incement by one.
+Bugs: 
+'''
+def dictionary_update(WorkingDict: dict, NewSca: int) -> dict:
+    pass
 
+def main() -> int:
+    print("Starting... ")
+    CumulativeDictionary = {}
     Barcode = input("Scan barcode:  ")
     Barcode = clean_barcode(Barcode)
     print(Barcode)
     print("Done!")
+    return 0
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()    
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
