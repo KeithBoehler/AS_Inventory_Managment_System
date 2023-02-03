8560f403b0ffb9b5339e84d6d3916c2c65f61173
crisjustcris <57770446+crisjustcris@users.noreply.github.com>
2023-02-03 11:37:14 -0600

    We found that there was no delete / decrement function so we created a function called undo_scan

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Keith Boehler, 23 Jan 2023
 import sys
 import os
 import csv
@@ -27,7 +26,24 @@ def peek_dictionary(Dicts: dict) -> None:
     '''
     for Keys, Values in Dicts.items():
         print(Keys, Values)
-    print("\n")
+        print("\n")
+
+def undo_scan(Dictionary2Correct, Barcode2Correct, Ammount2Correct=1):
+    '''
+    Purpose: Delete extra / erroneous barcode scans with the ability to choose how many
+    Precondition: loaded dictionary (Dictionary2Correct) and a barcode string (Barcode2Correct) to be deleted.
+    with a specified amount of barcode scans user wants to delte
+    Postcondition: Dictionary2Correct will be updated by deleting an entry if the value is one or decrementing if value > 1
+    '''
+    if Barcode2Correct in Dictionary2Correct.keys():
+        if Dictionary2Correct[Barcode2Correct] > 1:
+            Dictionary2Correct[Barcode2Correct] = Dictionary2Correct[Barcode2Correct] - Ammount2Correct
+        else:
+            del Dictionary2Correct[Barcode2Correct]
+    else:
+        print("Corrected barcode is not in dictionary... ")
+
+    return Dictionary2Correct
 
 
 def main() -> int:
@@ -35,6 +51,7 @@ def main() -> int:
     CumulativeDictionary = {}
     MenuOptions = {"Show menu again: ": "m",
                    "Show scans: ": "p",
+                   "Delete scans: ": "d",
                    "Write to csv file: ": "w",
                    "Exit program: ": "q"
                     }
@@ -58,11 +75,22 @@ def main() -> int:
         elif Barcode == 'm':
             print("\n")
             peek_dictionary(MenuOptions)
+        elif Barcode == 'd':
+            ErrorBarcode = input("Barcode you want to delete: ")
+            DecrimentQuantity = int(input("Amount you want to delete: "))
+            CumulativeDictionary = undo_scan(CumulativeDictionary, ErrorBarcode, DecrimentQuantity)
         else:
             CumulativeDictionary = dictionary_update(CumulativeDictionary, Barcode)
     
     return 0
-
+"""
+Bugs: NO ERROR HANDLING
+In the future we would like to implement:
+    error handling: unknown inputs such as ENTER, and other keys with no assigned function
+    Auto Saving: save write to disk every so often
+    Counter: if every counter modulo 5 write to disk0
+    Load CSV: in case you don't finish scanning have a file you can load for later
+"""
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()    
